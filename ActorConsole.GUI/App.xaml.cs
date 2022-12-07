7f18285790c4a6a93b19562cd476e86472b2bd93
kruumy <joshuampauls@gmail.com>
2022-12-07 12:09:23 -0500

    added messagebox on error

--- ActorConsole.GUI/App.xaml.cs
+++ ActorConsole.GUI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 
 namespace ActorConsole.GUI
 {
@@ -7,5 +9,12 @@ namespace ActorConsole.GUI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string ErrorDumpPath = Path.Combine(Environment.CurrentDirectory, $"error_{DateTime.Now:yyyy-dd-M--HH-mm-ss}.txt");
+            File.WriteAllText(ErrorDumpPath, e.Exception.ToString());
+            MessageBox.Show($"Error:\n\n{e.Exception.Message}\n\nWrote full error details to:\n\n\"{ErrorDumpPath}\"", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
