ec86a5f48534a3a756203668b1be9ddbbb0433f0
kruumy <joshuampauls@gmail.com>
2023-03-25 15:07:03 -0400

    removed senddvarstack for better solution

--- ActorConsole.Core/Memory/IW4.cs
+++ ActorConsole.Core/Memory/IW4.cs
@@ -1,10 +1,19 @@
-﻿using System.Diagnostics;
+﻿using ActorConsole.Core.Utils;
+using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace ActorConsole.Core.Memory
 {
     public static class IW4
     {
+        public static QueuedCooldown<string> DvarQueue = new QueuedCooldown<string>(new TimeSpan(0, 0, 0, 1, 0));
+
+        static IW4()
+        {
+            DvarQueue.OnInvoke += DvarQueue_OnInvoke;
+        }
+
         public static Process Game => Process.GetProcesses().FirstOrDefault(p => p.ProcessName == "iw4m" || p.ProcessName == "iw4x");
         public static bool IsRunning => Game != null;
 
@@ -12,9 +21,15 @@ namespace ActorConsole.Core.Memory
         {
             foreach ( string dvar in dvars )
             {
-                SendDvarStack.Stack.Push(dvar);
+                DvarQueue.Invoke(dvar);
             }
-            //TODO have always running class so no need to realloc whole rpc instructions every call.
+            //TODO remove reallocing whole rpc instructions every call. and just realloc args
+        }
+
+        private static void DvarQueue_OnInvoke( object sender, string dvar )
+        {
+            Console.WriteLine(dvar);
+            AnotherExternalMemoryLibrary.RemoteProcedureCall.Callx86(IW4.Game.Handle, Addresses.Cbuf_AddText, 0u, 0, dvar);
         }
     }
 }
---
+++ ActorConsole.Core/Utils/QueuedCooldown.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ActorConsole.Core.Utils
+{
+    public class QueuedCooldown
+    {
+        public QueuedCooldown( TimeSpan cooldown )
+        {
+            this.Cooldown = cooldown;
+        }
+
+        public event EventHandler OnInvoke;
+
+        public TimeSpan Cooldown { get; }
+        public int Overflow { get; private set; } = 0;
+
+        public async void Invoke()
+        {
+            Overflow++;
+            if ( Overflow == 1 )
+            {
+                while ( Overflow > 0 )
+                {
+                    OnInvoke?.Invoke(this, EventArgs.Empty);
+                    await Task.Delay(Cooldown);
+                    Overflow--;
+                }
+            }
+        }
+    }
+
+    public class QueuedCooldown<T>
+    {
+        public QueuedCooldown( TimeSpan cooldown )
+        {
+            this.Cooldown = cooldown;
+        }
+
+        public event EventHandler<T> OnInvoke;
+
+        public TimeSpan Cooldown { get; }
+        public Queue<T> Overflow { get; private set; } = new Queue<T>();
+
+        public async void Invoke( T arg )
+        {
+            Overflow.Enqueue(arg);
+            if ( Overflow.Count == 1 )
+            {
+                while ( Overflow.Count > 0 )
+                {
+                    OnInvoke?.Invoke(this, Overflow.Peek());
+                    await Task.Delay(Cooldown);
+                    Overflow.Dequeue();
+                }
+            }
+        }
+    }
+}
