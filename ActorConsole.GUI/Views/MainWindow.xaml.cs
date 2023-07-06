0b07258043eb5a70bba8a4337c938316016c63fc
kruumy <joshuampauls@gmail.com>
2023-07-06 19:45:04 -0400

    Sun tab disables itself in game does not support it

--- ActorConsole.GUI/Views/MainWindow.xaml.cs
+++ ActorConsole.GUI/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ActorConsole.GUI.Views
@@ -6,14 +7,26 @@ namespace ActorConsole.GUI.Views
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            Core.Manager.Instance.PropertyChanged += Instance_PropertyChanged;
+        }
+
+        private void Instance_PropertyChanged( object sender, PropertyChangedEventArgs e )
+        {
+            if ( e.PropertyName == nameof(Core.Manager.Game) )
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DoesGameHaveSunSupport)));
+            }
         }
 
         public ActorManager actorManagerProperty => actorManager;
+        public bool DoesGameHaveSunSupport => Core.Manager.Instance.Game is Core.Memory.IW4;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         private void TabItem_IsEnabledChanged( object sender, DependencyPropertyChangedEventArgs e )
         {
